ae26b1d65dd257c47531456bf06e3ba6cc11db8e
guodonghan <2532771263@qq.com>
2023-05-08 22:24:33 +0800

    add files

--- ch9/bank3/bank3.go
+++ ch9/bank3/bank3.go
@@ -17,5 +17,21 @@ func Balance() int {
 	mu.Lock()
 	b := balance
 	mu.Unlock()
+	//defer mu.Unlock()
 	return b
+}
+
+func WithDraw(amount int) bool {
+	mu.Lock()
+	defer mu.Unlock()
+	//Deposit(-amount)
+	if Balance() < 0 {
+		Deposit(amount)
+		return false
+	}
+	return true
+}
+
+func deposit(amount int) {
+	balance += amount
 }
