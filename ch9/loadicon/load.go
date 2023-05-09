8ece611d8c455da922a985e51b5307ded972ec9d
joedlut <253271263@qq.com>
2023-05-09 23:30:32 +0800

    add files

---
+++ ch9/loadicon/load.go
@@ -0,0 +1,20 @@
+package load
+
+var mu sync.Mutex
+
+var icons map[string]image.Image
+
+func loadIcons(){
+    icons := make(map[string]image.Image)
+    icons["1.png"]=loadIcon("1.png")
+    icons["2.png"]=loadIcon("2.png")
+}
+
+func Icon(name string)image.Image{
+    mu.Lock()
+    defer mu.UnLock()
+    if icons == nil{
+	    loadIcons()
+    }
+    return icons[name]
+}
