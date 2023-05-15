93f5219791536d2b10fcbed93885d177f3f4d77f
guodonghan <2532771263@qq.com>
2023-05-15 20:12:12 +0800

    add files

--- ch6/func_variable/variable.go
+++ ch6/func_variable/variable.go
@@ -14,10 +14,12 @@ func (r *Rocket) Launch() { fmt.Println("rocket launch") }
 func main() {
 	var n sync.WaitGroup
 	r := new(Rocket)
+	//r.Launch()
 	n.Add(1)
 	go func() {
 		defer n.Done()
 		time.AfterFunc(3*time.Second, r.Launch)
 	}()
+	//go time.AfterFunc(3*time.Second, r.Launch)
 	n.Wait()
 }
