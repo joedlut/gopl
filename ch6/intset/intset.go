9ec4ce24b852709c4fc2c2f81517807adbcb35e8
guodonghan <guodonghan@tencent.com>
2023-05-18 20:08:36 +0800

    add files

--- ch6/intset/intset.go
+++ ch6/intset/intset.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
 
 type IntSet struct {
 	words []uint64
@@ -11,8 +14,60 @@ func (s *IntSet) Has(x int) bool {
 	return word < len(s.words) && s.words[word]&(1<<bit) != 0
 }
 
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+func (s *IntSet) Union(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(t.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+
 func main() {
-	i := IntSet{[]uint64{12, 34, 567}}
-	fmt.Println(i.Has(23))
-	fmt.Println(i.Has(34))
+	var x, y IntSet
+	x.Add(1)
+	x.Add(34)
+	y.Add(23)
+	y.Add(1)
+	fmt.Println(x.Has(1))
+	fmt.Println(x.Has(34))
+	fmt.Println(x.Has(2324))
+	x.Union(&y)
+	fmt.Println(x.String())
+	fmt.Println(y.String())
+	fmt.Println(x)
+	fmt.Println(&x)
+	s := 2
+	s |= 4
+	fmt.Println(s)
+
 }
