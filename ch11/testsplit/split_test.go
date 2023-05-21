cb7afde3940e7985b087474d3ed1707a5e10c5cc
joedlut <2532771263@qq.com>
2023-05-21 22:30:41 +0800

    add files

---
+++ ch11/testsplit/split_test.go
@@ -0,0 +1,14 @@
+package testsplit
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestSplit(t *testing.T) {
+	s, sep := "a:b:c", ":"
+	words := strings.Split(s, sep)
+	if got, want := len(words), 3; got != want {
+		t.Errorf("Split(%q,%q) returned %d words,want %d", s, sep, got, want)
+	}
+}
